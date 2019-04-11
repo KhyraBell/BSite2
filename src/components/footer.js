f8357b64e1e5ac73a8f7a7c6fd853ff529366646
Michael Caveney <dylanesque27@gmail.com>
2019-04-11 12:10:17 -0400

    Began customization of footer component

---
+++ src/components/footer.js
@@ -0,0 +1,22 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import styled from "styled-components"
+
+const Footer = () => (
+  <footer>
+    <FooterLinks>
+      <Link>
+      <Link>
+      <Link>
+    </FooterLinks>
+  </footer>
+);
+
+const FooterLinks = styled.nav`
+  display: flex;
+`;
+
+
+
+export default Footer
