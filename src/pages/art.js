2b10522dceb7b3041f1984ce999fea6a21de6d23
Michael Caveney <dylanesque27@gmail.com>
2019-04-16 21:10:40 -0400

    Began art page content

--- src/pages/art.js
+++ src/pages/art.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 import Layout from "../components/layout"
+import chloe from "../images/chloe.png";
+import heroes from "../images/heroes.png";
+import poppies from "../images/poppies.png";
 
 const Art = () => {
   return (
     <Layout>
-    <div>I am the art page</div>
+    <div className="paragraph">
+      <h2>My Art</h2>
+
+      <div><h3>The Beauty of Poppies</h3>
+      <img src={poppies} alt="Cover Image from The Beauty of Poppies"/>
+      <p>A choice-driven RPG about the American War in Afghanistan. Download and unzip a playable sample here.</p>
+      </div>
+      <div><h3>The Rub</h3>
+      <iframe src="https://player.vimeo.com/video/210433188" frameBorder="0" />
+        <p>The short sequel to the upcoming feature, Intimates.</p>
+      </div>
+      <div><h3>Chloe Chamonix</h3>
+<img src={chloe} alt="Cover Image to Chloe Chamonix novel"/>
+      My urban fantasy about a girl detective. Download it from my store!
+      </div>
+      <div><h3>Heroes All</h3>
+<img src="" alt=""/>
+      </div>
+    </div>
     </Layout> );
 }
 
