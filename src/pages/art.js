189061445d55f3887bcaee69538bf11d1c116397
Michael Caveney <dylanesque27@gmail.com>
2019-04-20 12:15:50 -0400

    Began implementation of art page layout

--- src/pages/art.js
+++ src/pages/art.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import chloe from "../images/chloe.png";
-import heroes from "../images/heroes.png";
-import poppies from "../images/poppies.png";
+import chloe from "../images/chloe.png"
+import heroes from "../images/heroes.png"
+import poppies from "../images/poppies.png"
 
 const Art = () => {
   return (
     <Layout>
-    <div className="paragraph">
-      <h2>My Art</h2>
+      <div className="paragraph flex art-block">
+        <h2>My Art</h2>
 
-      <div><h3>The Beauty of Poppies</h3>
-      <img src={poppies} alt="Cover Image from The Beauty of Poppies"/>
-      <p>A choice-driven RPG about the American War in Afghanistan. Download and unzip a playable sample here.</p>
+        <div className="flex">
+          <h3>The Beauty of Poppies</h3>
+          <img
+            src={poppies}
+            className="image"
+            alt="Cover Image from The Beauty of Poppies"
+          />
+          <p>
+            A choice-driven RPG about the American War in Afghanistan. Download
+            and unzip a playable sample here.
+          </p>
+        </div>
+        <div className="flex">
+          <h3>The Rub</h3>
+          <iframe
+            src="https://player.vimeo.com/video/210433188"
+            frameBorder="0"
+          />
+          <p>The short sequel to the upcoming feature, Intimates.</p>
+        </div>
+        <div className="flex">
+          <h3>Chloe Chamonix</h3>
+          <img
+            src={chloe}
+            className="book"
+            alt="Cover Image to Chloe Chamonix novel"
+          />
+          <p>
+            My urban fantasy about a girl detective. Download it from my{" "}
+            <Link to="/store">store</Link>.
+          </p>
+        </div>
+        <div className="flex">
+          <h3>Heroes All</h3>
+          <img src={heroes} className="image" alt="Heroes All RPG cover" />
+        </div>
       </div>
-      <div><h3>The Rub</h3>
-      <iframe src="https://player.vimeo.com/video/210433188" frameBorder="0" />
-        <p>The short sequel to the upcoming feature, Intimates.</p>
-      </div>
-      <div><h3>Chloe Chamonix</h3>
-<img src={chloe} alt="Cover Image to Chloe Chamonix novel"/>
-      My urban fantasy about a girl detective. Download it from my store!
-      </div>
-      <div><h3>Heroes All</h3>
-<img src="" alt=""/>
-      </div>
-    </div>
-    </Layout> );
+    </Layout>
+  )
 }
 
-export default Art;
+export default Art
